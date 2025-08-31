f8fb33dd67af5acf9b3e3d00c6996c795cae4ed0
greemaltend4 <dengestabiy@hotmail.com>
2025-08-31 10:06:47 -0500

    Fix typo in response message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue dans votre outil d’intégration cloud !');
+  res.send('Bienvenue dans votre outil d\u2019int\u00e9gration cloud !');
 });
 
 app.listen(port, () => {
